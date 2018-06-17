c8a93c39c9a8f0e6b3ad1caf0a9e34480c112c4c
Ashutosh Vaish <ashutoshvaish6397@gmail.com>
2018-06-17 15:57:33 +0530

    Update OpenDataCube_Product_Loader_dialog.py

--- opendatacubeproductloaderplugin/OpenDataCube_Product_Loader_dialog.py
+++ opendatacubeproductloaderplugin/OpenDataCube_Product_Loader_dialog.py
@@ -8,16 +8,16 @@
                              -------------------
         begin                : 2018-05-06
         git sha              : $Format:%H$
-        copyright            : (C) 2018 by Ashu
+        copyright            : (C) 2018 by Ashutosh Vaish
         email                : ashutoshvaish6397@gmail.com
  ***************************************************************************/
 
 /***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   it under the terms of the GNU General Public License v3.0 as published*
+ *   by the Free Software Foundation;                                      *
+ *                                                                         *
  *                                                                         *
  ***************************************************************************/
 """
